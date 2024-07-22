a697c80fa80c4d96592223e623d848a0a4174b91
Shafana <shafana8129@gmail.com>
2024-07-22 10:33:58 +0530

    Routes created

---
+++ api/routes/user.route.js
@@ -0,0 +1,26 @@
+import express from "express";
+
+const app=express();
+
+app.get('/get-all-notes',(req, res) => {
+    // To get all notes
+});
+
+app.post('/',(req, res) => {
+    // To create new note
+});
+
+app.patch('/',(req, res) => {
+    // To edit a note
+});
+
+app.delete('/',(req, res) => {
+    // To delete a note
+});
+
+
+
+
+
+
+
