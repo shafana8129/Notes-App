1333fd5464299090f0c61386ea7e4f7b4bddc946
Shafana <shafana8129@gmail.com>
2024-08-05 14:34:27 +0530

    MongoDb connected, Nodemon setupped

--- api/index.js
+++ api/index.js
@@ -1,9 +1,17 @@
 import express from "express";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const port = 3000;
-
 const app = express();
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
+})
+
+mongoose.connect(process.env.MONGODB).then(()=> {
+    console.log('MongoDb Connected');
+}).catch((e) => {
+    console.log('MongoDb Connection failed!',e);
 })
